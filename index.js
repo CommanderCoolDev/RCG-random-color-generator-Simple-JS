2df20c9e94ce400ea8587170a8293de343ed9168
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-10-19 18:51:49 +0300

    fixed RCG

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const currentColor = document.querySelector('.current-collor');
+const currentColor = document.querySelector('.current-color');
 const bodyElem = document.querySelector('body');
 const generateBtn = document.querySelector('.generate-btn');
+// console.log('currentColor.innerText :>> ', currentColor);
+// console.log('bodyElem :>> ', bodyElem);
+// console.log('generateBtn :>> ', generateBtn);
 
 bodyElem.style.backgroundColor = currentColor.innerText;
 generateBtn.addEventListener('click', () => {
     let randomColor = '';
     let chaaracters = '0123456789abcdef';
 
-    for (let i=0; i < 6; i+=1) {
+    for (let i=0; i < 6; i++) {
         randomColor = randomColor + chaaracters[Math.floor(Math.random() * 16)];
 
     }
